4e33b6bfe39c5385486a4d5e2f630b5227b82a5c
Pablo <tackpablo@gmail.com>
2022-11-19 07:13:12 -0800

    lesson 5 countDiv

---
+++ Lesson 5 Prefix Sums/countDiv.js
@@ -0,0 +1,24 @@
+// %
+function solution(A, B, K) {
+  let count = A % K == 0 ? 1 : 0;
+  return count + (Math.floor(B / K) - Math.floor(A / K));
+}
+
+// O() - space
+// O() - time
+
+// 50%
+function solution(A, B, K) {
+  let count = 0;
+
+  for (let i = A; i <= B; i++) {
+    if (i % K === 0) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// O(1) - space
+// O(n) - time
